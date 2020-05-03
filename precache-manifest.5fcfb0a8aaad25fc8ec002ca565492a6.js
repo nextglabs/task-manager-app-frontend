69c2a4dd1f5f46f4f045ffdcaf805bd96d766e1d
Bassem <bassem@coradine.com>
2020-05-03 14:28:33 +0200

    Updates

---
+++ precache-manifest.5fcfb0a8aaad25fc8ec002ca565492a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd70f844094bae2d9aeb868152794e83",
+    "url": "/task-manager-app-frontend/index.html"
+  },
+  {
+    "revision": "916bdacf5b4d5d9c414e",
+    "url": "/task-manager-app-frontend/static/css/main.f34faff2.chunk.css"
+  },
+  {
+    "revision": "c855c97f80092735023c",
+    "url": "/task-manager-app-frontend/static/js/2.9a0cfc35.chunk.js"
+  },
+  {
+    "revision": "e8c805f382e3ba1da0dbc0cf6629c77f",
+    "url": "/task-manager-app-frontend/static/js/2.9a0cfc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916bdacf5b4d5d9c414e",
+    "url": "/task-manager-app-frontend/static/js/main.ee6b9e4c.chunk.js"
+  },
+  {
+    "revision": "1b71a3f2ce9ac09e194e",
+    "url": "/task-manager-app-frontend/static/js/runtime-main.bc4b0815.js"
+  }
+]);
